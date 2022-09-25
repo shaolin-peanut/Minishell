7b8a49049fbd4cbfb8671026aeb165743291282f
Glanfaloth <yeltao@student.ethz.ch>
2022-09-25 21:48:10 +0200

    improve prompt

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: lanlan <lanlan@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/27 13:27:35 by lanlan            #+#    #+#             */
-/*   Updated: 2022/09/10 22:05:56 by lanlan           ###   ########.fr       */
+/*   Updated: 2022/09/25 21:46:42 by lanlan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,27 +20,6 @@ static t_prompt	init_prompt(char **argv, char **envp)
 	prompt.commands = NULL;
 	prompt.envp = ft_matrixdup(envp);
 	return (prompt);
-}
-
-char	*get_prompt(t_prompt prompt)
-{
-	char	*temp;
-	char	*temp2;
-	char	*home;
-
-	(void) prompt;
-	// temp = get_user(prompt);
-	temp = ft_strdup("lanlan");
-	temp2 = ft_strjoin(temp, "@minishell ");
-	free(temp);
-	// home = get_home(prompt);
-	home = getcwd(NULL, 0);
-	temp = ft_strjoin(temp2, home);
-	free(home);
-	free(temp2);
-	temp2 = ft_strjoin(temp, "$ ");
-	free(temp);
-	return (temp2);
 }
 
 int	main(int argc, char **argv, char **envp)
--- src/parser/prompt.c
+++ src/parser/prompt.c
@@ -6,10 +6,45 @@
 /*   By: lanlan <lanlan@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/27 17:29:42 by lanlan            #+#    #+#             */
-/*   Updated: 2022/08/27 17:53:10 by lanlan           ###   ########.fr       */
+/*   Updated: 2022/09/25 21:47:37 by lanlan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minishell.h"
 
+static char	*get_home(t_prompt prompt)
+{
+	char	*pwd;
+	char	*home;
 
+	pwd = getcwd(NULL, 0);
+	if (!pwd)
+		pwd = ft_strdup("∅ ");
+	home = ft_strdup("/Users/lanlan"); // TODO: from env
+    (void) prompt; // TODO: useful for getting home from env
+	if (home && home[0] && ft_strnstr(pwd, home, ft_strlen(pwd)))
+		pwd = ft_strjoin("~", &pwd[ft_strlen(home)]);
+	free(home);
+	home = ft_strjoin(pwd, " ");
+	free(pwd);
+	pwd = ft_strjoin(" ", home);
+	free(home);
+	return (pwd);
+}
+
+char	*get_prompt(t_prompt prompt)
+{
+	char	*temp;
+	char	*username;
+	char	*home;
+	char	*prompt_str;
+
+	username = ft_strjoin("guest", "@minishell");
+	home = get_home(prompt);
+	temp = ft_strjoin(home, "$ ");
+	prompt_str = ft_strjoin(username, temp);
+	free(username);
+	free(home);
+	free(temp);
+	return (prompt_str);
+}
